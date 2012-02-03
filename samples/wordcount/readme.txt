5458e77c01c9bbebaf8f16450ce3914dafef7b32
Costin Leau <cleau@vmware.com>
2012-02-03 14:59:47 +0200

    improve and cleanup sample
    replace Maven with Gradle
    add readme.txt
    make test work on Windows
    minimize logging
    prettify XML configs

---
+++ samples/wordcount/readme.txt
@@ -0,0 +1,26 @@
+====================
+== WordCount Demo ==
+====================
+
+1. MOTIVATION
+
+A basic word count demo that illustrates the configuration
+and interaction with the Hadoop through Spring Hadoop.
+
+The demo counts the word from a given resource through a Map Reduce job.
+
+2. BUILD AND DEPLOYMENT
+
+This directory contains the source files.
+For building, JDK 1.6+ are required
+
+To build the sample, use the following command:
+
+*nix/BSD OS:
+# ../../gradlew
+
+Windows OS:
+# ..\..\gradlew
+
+If you have Gradle installed and available in your classpath, you can simply type:
+# gradle
--- samples/wordcount/src/test/java/org/springframework/data/hadoop/samples/WordCountWorkflowTests.java
+++ samples/wordcount/src/test/java/org/springframework/data/hadoop/samples/WordCountWorkflowTests.java
@@ -1,34 +1,44 @@
-package org.springframework.data;
+package org.springframework.data.hadoop.samples;
 
-import java.io.File;
 import java.util.Map;
 
-import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.FileSystem;
-import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.mapreduce.JobSubmissionFiles;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.batch.core.BatchStatus;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.beans.factory.BeanInitializationException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.GenericXmlApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("/launch-context.xml")
 public class WordCountWorkflowTests {
+
+    @Autowired
+    private ApplicationContext ctx;
+
+	// hack to make Hadoop FS 'work' on Windows
+	private static void hackHadoopStagingOnWin() {
+		// do the assignment only on Windows systems
+		if (System.getProperty("os.name").startsWith("Windows")) {
+			// 0655 = -rwxr-xr-x
+			JobSubmissionFiles.JOB_DIR_PERMISSION.fromShort((short) 0655);
+			JobSubmissionFiles.JOB_FILE_PERMISSION.fromShort((short) 0655);
+		}
+	}
+
+	{
+		hackHadoopStagingOnWin();
+	}
 
 	@Test
 	public void testWorkflowNS() throws Exception {
-		GenericXmlApplicationContext ctx = new GenericXmlApplicationContext(
-				"/launch-context.xml");
-
-		ctx.registerShutdownHook();
-		//to keep dev and runtime deployment configuration the same
-		System.setProperty("basedir", "src" + File.separator + "main");
-		//FileSystem fs = FileSystem.get(ctx.getBean(Configuration.class));
-		//fs.delete(new Path("/user/gutenberg/output/word/"), true);
-
-		
 		startJobs(ctx);
 	}
 	
@@ -47,5 +57,4 @@ public class WordCountWorkflowTests {
 			}
 		}
 	}
-
 }
