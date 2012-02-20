21f15342c98e48d88557d30e2c9f7001904c65d4
Costin Leau <cleau@vmware.com>
2012-02-20 21:06:04 +0200

    add initial hdfs item reader impl

---
+++ src/test/java/org/springframework/data/hadoop/batch/HdfsItemReaderTest.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2011-2012 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.data.hadoop.batch;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.batch.item.ExecutionContext;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author Costin Leau
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration
+public class HdfsItemReaderTest {
+
+	@Autowired
+	ApplicationContext ctx;
+	@Autowired
+	HdfsItemReader reader;
+
+	@Test
+	public void testReader() throws Exception {
+		try {
+			reader.open(new ExecutionContext());
+			assertNotNull(reader.read());
+		} finally {
+			reader.close();
+		}
+	}
+}
