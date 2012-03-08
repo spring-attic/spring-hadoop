0b616d531d3625ded78f9d5279ba49074a69dc9d
Jarred Li <leejianwei@126.com>
2012-03-08 08:41:06 +0800

    update hbase example

--- samples/hbase/src/main/java/org/springframework/data/hadoop/samples/hbase/CellValueCountReducer.java
+++ samples/hbase/src/main/java/org/springframework/data/hadoop/samples/hbase/CellValueCountReducer.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2011 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.springframework.data.hadoop.samples.hbase;
 
 import java.io.IOException;
@@ -9,7 +25,12 @@ import org.apache.hadoop.hbase.util.Bytes;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 
-
+/**
+ * Reducer
+ * 
+ * @author Jarred Li
+ * 
+ */
 public class CellValueCountReducer extends
 		TableReducer<Text, IntWritable, ImmutableBytesWritable> {
 
@@ -20,8 +41,8 @@ public class CellValueCountReducer extends
 			i += val.get();
 		}
 		Put put = new Put(Bytes.toBytes(key.toString()));
-		put.add(Bytes.toBytes("cf"), Bytes.toBytes("count"),
-				Bytes.toBytes("tatal is:" + i));
+		put.add(Bytes.toBytes(Constant.columnFamilyName),
+				Bytes.toBytes("count"), Bytes.toBytes("tatal is:" + i));
 		context.write(null, put);
 	}
 
