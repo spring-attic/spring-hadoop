f7067992615792315b6bb150548244d235c89ad4
Janne Valkealahti <janne.valkealahti@gmail.com>
2013-08-30 13:18:21 +0100

    SHDP-170 Adding command line client runner
    CommandLineClientRunner is following same logic familiar
    from appmaster and container runners. This is an implementation
    which can be either run from a command line or extended with
    custom static main method if command line arguments needs to be
    hardcoded or tweaked. Also adding console implementation from
    spring-xd to make table type listing more pretty.

---
+++ spring-yarn/spring-yarn-core/src/main/java/org/springframework/yarn/client/CommandLineClientRunner.java
@@ -0,0 +1,150 @@
+/*
+ * Copyright 2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.yarn.client;
+
+import java.text.DateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Properties;
+import java.util.Set;
+
+import org.apache.hadoop.yarn.api.records.ApplicationId;
+import org.apache.hadoop.yarn.api.records.ApplicationReport;
+import org.springframework.util.StringUtils;
+import org.springframework.yarn.YarnSystemConstants;
+import org.springframework.yarn.launch.AbstractCommandLineRunner;
+import org.springframework.yarn.launch.ExitStatus;
+import org.springframework.yarn.support.console.Table;
+import org.springframework.yarn.support.console.TableHeader;
+import org.springframework.yarn.support.console.TableRow;
+import org.springframework.yarn.support.console.UiUtils;
+
+/**
+ * A simple client runner executing a bean named "yarnClient".
+ *
+ * @author Janne Valkealahti
+ *
+ */
+public class CommandLineClientRunner extends AbstractCommandLineRunner<YarnClient> {
+
+	public final static String OPT_SUBMIT = "-submit";
+	public final static String OPT_KILL = "-kill";
+	public final static String OPT_LIST = "-list";
+	public final static String ARG_APPLICATION_ID = "applicationId";
+
+	@SuppressWarnings("serial")
+	private final static List<String> opts = new ArrayList<String>() {{
+	    add(OPT_SUBMIT);
+	    add(OPT_KILL);
+	    add(OPT_LIST);
+	}};
+
+	@Override
+	protected ExitStatus handleBeanRun(YarnClient bean, String[] parameters, Set<String> opts) {
+		Properties properties = StringUtils.splitArrayElementsIntoProperties(parameters, "=");
+
+		if (opts.contains(OPT_SUBMIT)) {
+			bean.submitApplication();
+		} else if (opts.contains(OPT_LIST)) {
+			printApplicationsReport(bean.listApplications());
+		} else if (opts.contains(OPT_KILL)) {
+			ApplicationId appId = queryApplicationId(bean,
+					properties != null ? properties.getProperty(ARG_APPLICATION_ID) : null);
+			if (appId != null) {
+				bean.killApplication(appId);
+			}
+		}
+
+		return ExitStatus.COMPLETED;
+	}
+
+	@Override
+	protected String getDefaultBeanIdentifier() {
+		return YarnSystemConstants.DEFAULT_ID_CLIENT;
+	}
+
+	@Override
+	protected List<String> getValidOpts() {
+		return opts;
+	}
+
+	/**
+	 * Query application id.
+	 *
+	 * @param client the yarn client
+	 * @param applicationId the application id
+	 * @return the application id if exists, NULL otherwise
+	 */
+	protected ApplicationId queryApplicationId(YarnClient client, String applicationId) {
+		if (!StringUtils.hasText(applicationId)) {
+			return null;
+		}
+		ApplicationId appId = null;
+		for (ApplicationReport a : client.listApplications()) {
+			if (a.getApplicationId().toString().equals(applicationId)) {
+				appId = a.getApplicationId();
+				break;
+			}
+		}
+		return appId;
+	}
+
+	/**
+	 * Prints the applications report into system out.
+	 *
+	 * @param applications the applications
+	 */
+	private static void printApplicationsReport(List<ApplicationReport> applications) {
+		System.out.println(UiUtils.renderTextTable(getApplicationReportTable(applications), true));
+	}
+
+	/**
+	 * Gets the application report table.
+	 *
+	 * @param applications the applications
+	 * @return the application report table
+	 */
+	private static Table getApplicationReportTable(List<ApplicationReport> applications) {
+		Table table = new Table();
+		table.addHeader(1, new TableHeader("Id"))
+				.addHeader(2, new TableHeader("User"))
+				.addHeader(3, new TableHeader("Name"))
+				.addHeader(4, new TableHeader("Queue"))
+				.addHeader(5, new TableHeader("StartTime"))
+				.addHeader(6, new TableHeader("FinishTime"))
+				.addHeader(7, new TableHeader("State"))
+				.addHeader(8, new TableHeader("FinalStatus"));
+
+		for (ApplicationReport a : applications) {
+			final TableRow row = new TableRow();
+			row.addValue(1, a.getApplicationId().toString())
+					.addValue(2, a.getUser())
+					.addValue(3, a.getName())
+					.addValue(4, a.getQueue())
+					.addValue(5, DateFormat.getDateTimeInstance(DateFormat.SHORT, DateFormat.SHORT).format(
+							new Date(a.getStartTime())))
+					.addValue(6, DateFormat.getDateTimeInstance(DateFormat.SHORT, DateFormat.SHORT).format(
+							new Date(a.getFinishTime())))
+					.addValue(7, a.getYarnApplicationState().toString())
+					.addValue(8, a.getFinalApplicationStatus().toString());
+			table.getRows().add(row);
+		}
+		return table;
+	}
+
+
+}
---
+++ spring-yarn/spring-yarn-core/src/main/java/org/springframework/yarn/support/console/CommonUtils.java
@@ -0,0 +1,121 @@
+/*
+ * Copyright 2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.yarn.support.console;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.util.Collection;
+
+import org.springframework.util.StringUtils;
+
+/**
+ * Contains common non-ui related helper methods for rendering text to the console.
+ *
+ * @author Gunnar Hillert
+ * @author Stephan Oudmaijer
+ * @author Janne Valkealahti
+ *
+ */
+public abstract class CommonUtils {
+
+	private static final String EMAIL_VALIDATION_REGEX = "^[_A-Za-z0-9-+]+(\\.[_A-Za-z0-9-+]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$";
+
+	/**
+	 * Right-pad a String with a configurable padding character.
+	 *
+	 * @param inputString The String to pad. A {@code null} String will be treated like an empty String.
+	 * @param size Pad String by the number of characters.
+	 * @param paddingChar The character to pad the String with.
+	 * @return The padded String. If the provided String is null, an empty String is returned.
+	 */
+	public static String padRight(String inputString, int size, char paddingChar) {
+
+		final String stringToPad;
+
+		if (inputString == null) {
+			stringToPad = "";
+		}
+		else {
+			stringToPad = inputString;
+		}
+
+		StringBuilder padded = new StringBuilder(stringToPad);
+		while (padded.length() < size) {
+			padded.append(paddingChar);
+		}
+		return padded.toString();
+	}
+
+	/**
+	 * Right-pad the provided String with empty spaces.
+	 *
+	 * @param string The String to pad
+	 * @param size Pad String by the number of characters.
+	 * @return The padded String. If the provided String is null, an empty String is returned.
+	 */
+	public static String padRight(String string, int size) {
+		return padRight(string, size, ' ');
+	}
+
+	/**
+	 * Convert a List of Strings to a comma delimited String.
+	 *
+	 * @param list
+	 * @return Returns the List as a comma delimited String. Returns an empty String for a Null or empty list.
+	 */
+	public static String collectionToCommaDelimitedString(Collection<String> list) {
+		return StringUtils.collectionToCommaDelimitedString(list);
+	}
+
+	/**
+	 * @param reader
+	 */
+	public static void closeReader(Reader reader) {
+		if (reader != null) {
+			try {
+				reader.close();
+			}
+			catch (IOException e) {
+				throw new IllegalStateException("Encountered problem closing Reader.", e);
+			}
+		}
+	}
+
+	/**
+	 * @param emailAddress The email address to validate
+	 * @return {@code true} if the provided email address is valid; {@code false} otherwise
+	 */
+	public static boolean isValidEmail(String emailAddress) {
+		return emailAddress.matches(EMAIL_VALIDATION_REGEX);
+	}
+
+	/**
+	 * Simple method to replace characters in a String with asterisks to mask the password.
+	 *
+	 * @param password The password to mask
+	 */
+	public static String maskPassword(String password) {
+		int lengthOfPassword = password.length();
+		StringBuilder stringBuilder = new StringBuilder(lengthOfPassword);
+
+		for (int i = 0; i < lengthOfPassword; i++) {
+			stringBuilder.append('*');
+		}
+
+		return stringBuilder.toString();
+	}
+
+}
