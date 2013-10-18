19c52156f46ec8d96b64797378a7f3eedff59af8
Janne Valkealahti <janne.valkealahti@gmail.com>
2013-10-18 14:07:03 +0100

    SHDP-190 Better concept for handling tests
    Returning both state and application id when submitting
    app via AbstractYarnClusterTests. Simplify finding log
    files from a minicluster.

---
+++ spring-yarn/spring-yarn-test-core/src/main/java/org/springframework/yarn/test/support/ContainerLogUtils.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright 2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.yarn.test.support;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.hadoop.yarn.api.records.ApplicationId;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.util.StringUtils;
+import org.springframework.yarn.test.context.YarnCluster;
+
+/**
+ * Utilities for checking container logs.
+ *
+ * @author Janne Valkealahti
+ *
+ */
+public abstract class ContainerLogUtils {
+
+	static Log log = LogFactory.getLog(ContainerLogUtils.class);
+
+	/**
+	 * Find container logs for running cluster and application.
+	 * <p>
+	 * Created pattern which is used with {@code PathMatchingResourcePatternResolver}
+	 * is resolved from {@code YarnCluster}, {@code ApplicationId} and fileName.
+	 * For example if fileName is given as <code>*.std*</code>, pattern will look like
+	 * <code>file:/path/to/project/target/yarn--1502101888/*logDir*&#47;application_1382082435804_0001&#47;**&#47;*.std*</code>
+	 *
+	 * @param yarnCluster the yarn cluster
+	 * @param applicationId the application id
+	 * @param fileName the part of a file name
+	 * @return the list of log file {@code Resource}s
+	 * @throws IOException Signals that an I/O exception has occurred.
+	 */
+	public static List<Resource> queryContainerLogs(YarnCluster yarnCluster, ApplicationId applicationId, String fileName)
+			throws IOException {
+		String lastPart = StringUtils.hasText(fileName) ? fileName : "*";
+		String pattern = "file:" + yarnCluster.getYarnWorkDir().getAbsolutePath()
+				+ "/*logDir*/" + applicationId.toString()
+				+ "/**/" + lastPart;
+		PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+		ArrayList<Resource> ret = new ArrayList<Resource>();
+		for (Resource r : resolver.getResources(pattern)) {
+			if (r.getFile().isFile()) {
+				ret.add(r);
+			}
+		}
+		return ret;
+	}
+
+	/**
+	 * Find container logs for running cluster and application. <code>fileName</code>
+	 * will be <code>*</code>.
+	 *
+	 * @param yarnCluster the yarn cluster
+	 * @param applicationId the application id
+	 * @return the list of log file {@code Resource}s
+	 * @throws IOException Signals that an I/O exception has occurred.
+	 * @see {@link #queryContainerLogs(YarnCluster, ApplicationId, String)}
+	 */
+	public static List<Resource> queryContainerLogs(YarnCluster yarnCluster, ApplicationId applicationId)
+			throws IOException {
+		return queryContainerLogs(yarnCluster, applicationId, null);
+	}
+
+}
