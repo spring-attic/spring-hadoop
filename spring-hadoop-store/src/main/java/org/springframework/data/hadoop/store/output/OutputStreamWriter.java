cd05043cdcc2c25c34d23065140889bd4d2839bf
Janne Valkealahti <janne.valkealahti@gmail.com>
2013-12-06 12:07:05 +0000

    SHDP-240 First take on writing byte arrays into a store

---
+++ spring-hadoop-store/src/main/java/org/springframework/data/hadoop/store/output/OutputStreamWriter.java
@@ -0,0 +1,109 @@
+/*
+ * Copyright 2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.data.hadoop.store.output;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.springframework.data.hadoop.store.DataStoreWriter;
+import org.springframework.data.hadoop.store.codec.CodecInfo;
+import org.springframework.data.hadoop.store.event.FileWrittenEvent;
+import org.springframework.data.hadoop.store.event.StoreEventPublisher;
+import org.springframework.data.hadoop.store.support.OutputContext;
+import org.springframework.data.hadoop.store.support.StreamsHolder;
+
+/**
+ * A {@code OutputStreamWriter} is a {@code DataStoreWriter} implementation
+ * able to write {@code byte[]}s into raw hdfs files.
+ *
+ * @author Janne Valkealahti
+ *
+ */
+public class OutputStreamWriter extends AbstractDataStreamWriter implements DataStoreWriter<byte[]> {
+
+	private final static Log log = LogFactory.getLog(OutputStreamWriter.class);
+
+	private StreamsHolder<OutputStream> streamsHolder;
+
+	/**
+	 * Instantiates a new output stream writer.
+	 *
+	 * @param configuration the configuration
+	 * @param basePath the base path
+	 * @param codec the codec
+	 */
+	public OutputStreamWriter(Configuration configuration, Path basePath, CodecInfo codec) {
+		super(configuration, basePath, codec);
+	}
+
+	@Override
+	public void flush() throws IOException {
+		if (streamsHolder == null) {
+			streamsHolder.getStream().flush();
+		}
+	}
+
+	@Override
+	public void close() throws IOException {
+		if (streamsHolder != null) {
+			streamsHolder.close();
+
+			renameFile(streamsHolder.getPath());
+
+			StoreEventPublisher storeEventPublisher = getStoreEventPublisher();
+			if (storeEventPublisher != null) {
+				storeEventPublisher.publishEvent(new FileWrittenEvent(this, streamsHolder.getPath()));
+			}
+
+			streamsHolder = null;
+		}
+	}
+
+	@Override
+	public void write(byte[] entity) throws IOException {
+		if (streamsHolder == null) {
+			streamsHolder = getOutput();
+		}
+		OutputStream out = streamsHolder.getStream();
+		out.write(entity);
+
+		setWritePosition(getPosition(streamsHolder));
+
+		OutputContext context = getOutputContext();
+		if (context.getRolloverState()) {
+			log.info("after write, rollever state is true");
+			close();
+			context.rollStrategies();
+		}
+
+	}
+
+	@Override
+	protected void handleIdleTimeout() {
+		log.info("Idle timeout detected for this writer, closing stream");
+		try {
+			close();
+		} catch (IOException e) {
+			log.error("error closing", e);
+		}
+		getOutputContext().rollStrategies();
+	}
+
+}
---
+++ spring-hadoop-store/src/test/java/org/springframework/data/hadoop/store/RawStreamStoreTests.java
@@ -0,0 +1,139 @@
+/*
+ * Copyright 2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.data.hadoop.store;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+import org.apache.hadoop.fs.Path;
+import org.junit.Test;
+import org.springframework.data.hadoop.store.codec.Codecs;
+import org.springframework.data.hadoop.store.input.TextFileReader;
+import org.springframework.data.hadoop.store.output.OutputStreamWriter;
+import org.springframework.data.hadoop.store.strategy.naming.ChainedFileNamingStrategy;
+import org.springframework.data.hadoop.store.strategy.naming.CodecFileNamingStrategy;
+import org.springframework.data.hadoop.store.strategy.naming.StaticFileNamingStrategy;
+import org.springframework.data.hadoop.store.support.StoreUtils;
+
+/**
+ * Tests for writing raw byte arrays.
+ *
+ * @author Janne Valkealahti
+ *
+ */
+public class RawStreamStoreTests extends AbstractStoreTests {
+
+	@Test
+	public void testWriteReadTextTwoLines() throws IOException {
+		String[] dataArray = new String[] { DATA10, DATA11 };
+
+		byte[][] data = new byte[4][];
+		data[0] = DATA10.getBytes();
+		data[1] = "\n".getBytes();
+		data[2] = DATA11.getBytes();
+		data[3] = "\n".getBytes();
+
+		OutputStreamWriter writer = new OutputStreamWriter(testConfig, testDefaultPath, null);
+		TestUtils.writeData(writer, data, true);
+
+		TextFileReader reader = new TextFileReader(testConfig, testDefaultPath, null);
+		TestUtils.readDataAndAssert(reader, dataArray);
+	}
+
+	@Test
+	public void testStreamSmall() throws IOException {
+		ByteArrayInputStream stream = new ByteArrayInputStream(DATA10.getBytes());
+		OutputStreamWriter writer = new OutputStreamWriter(testConfig, testDefaultPath, null);
+
+		doWithInputStream(stream, writer);
+
+		String[] dataArray = new String[] { DATA10 };
+		TextFileReader reader = new TextFileReader(testConfig, testDefaultPath, null);
+		TestUtils.readDataAndAssert(reader, dataArray);
+	}
+
+	@Test
+	public void testStreamBig() throws IOException {
+		StringBuilder buf = new StringBuilder();
+		for (int i = 0; i<1000; i++) {
+			buf.append(DATA10);
+			buf.append("\n");
+		}
+		ByteArrayInputStream stream = new ByteArrayInputStream(buf.toString().getBytes());
+		OutputStreamWriter writer = new OutputStreamWriter(testConfig, testDefaultPath, null);
+
+		doWithInputStream(stream, writer);
+
+		TextFileReader reader = new TextFileReader(testConfig, testDefaultPath, null);
+		List<String> data = TestUtils.readData(reader);
+		assertThat(data.size(), is(1000));
+	}
+
+	@Test
+	public void testStreamBigWithCodec() throws IOException {
+		StringBuilder buf = new StringBuilder();
+		for (int i = 0; i<1000; i++) {
+			buf.append(DATA10);
+			buf.append("\n");
+		}
+		ByteArrayInputStream stream = new ByteArrayInputStream(buf.toString().getBytes());
+		OutputStreamWriter writer = new OutputStreamWriter(testConfig, testDefaultPath, Codecs.GZIP.getCodecInfo());
+
+		doWithInputStream(stream, writer);
+
+		TextFileReader reader = new TextFileReader(testConfig, testDefaultPath, Codecs.GZIP.getCodecInfo());
+		List<String> data = TestUtils.readData(reader);
+		assertThat(data.size(), is(1000));
+	}
+
+	@Test
+	public void testStreamBigWithCodecWithStrategys() throws IOException {
+		StringBuilder buf = new StringBuilder();
+		for (int i = 0; i<1000; i++) {
+			buf.append(DATA10);
+			buf.append("\n");
+		}
+		ByteArrayInputStream stream = new ByteArrayInputStream(buf.toString().getBytes());
+		OutputStreamWriter writer = new OutputStreamWriter(testConfig, testDefaultPath, Codecs.GZIP.getCodecInfo());
+
+		ChainedFileNamingStrategy fileNamingStrategy = new ChainedFileNamingStrategy();
+		fileNamingStrategy.register(new StaticFileNamingStrategy("data"));
+		fileNamingStrategy.register(new CodecFileNamingStrategy());
+		writer.setFileNamingStrategy(fileNamingStrategy);
+
+		doWithInputStream(stream, writer);
+
+		TextFileReader reader = new TextFileReader(testConfig, new Path(testDefaultPath, "data.gzip"), Codecs.GZIP.getCodecInfo());
+		List<String> data = TestUtils.readData(reader);
+		assertThat(data.size(), is(1000));
+	}
+
+	/**
+	 * Kinda simulating RemoteFileTemplate
+	 */
+	private static void doWithInputStream(InputStream stream, DataStoreWriter<byte[]> writer) throws IOException {
+		// if used together with RemoteFileTemplate, DataStoreWriter could
+		// be set via Tasklet constructor and marked as final to be
+		// used within a InputStreamCallback
+		StoreUtils.copy(stream, writer);
+	}
+
+}
