30eaee75981f3dcde487bc624a9ca327b8885e7a
Thomas Risberg <trisberg@gopivotal.com>
2014-01-31 18:33:41 -0500

    SHDP-184 Fixing Hive tests

--- src/test/java/org/springframework/data/hadoop/hive/HiveBatchTest.java
+++ src/test/java/org/springframework/data/hadoop/hive/HiveBatchTest.java
@@ -30,6 +30,7 @@ import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.Resource;
 import org.springframework.data.hadoop.TestUtils;
 import org.springframework.data.hadoop.batch.JobsTrigger;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.util.ReflectionUtils;
@@ -42,6 +43,7 @@ import static org.junit.Assert.assertTrue;
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
 public class HiveBatchTest {
 
 	@Autowired
