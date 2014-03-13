201fa06fa65b35fdfc06e12866b9bda09e82386a
Janne Valkealahti <janne.valkealahti@gmail.com>
2014-03-13 11:58:56 +0000

    SHDP-180 suppress/fix hbase tests warnings

--- spring-hadoop-build-tests/src/test/java/org/springframework/data/hadoop/hbase/BasicHBaseTest.java
+++ spring-hadoop-build-tests/src/test/java/org/springframework/data/hadoop/hbase/BasicHBaseTest.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2011 the original author or authors.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -38,8 +38,8 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import static org.junit.Assert.*;
 
 /**
- * Test for basic HBase connectivity (based on the API usage in client package). 
- * 
+ * Test for basic HBase connectivity (based on the API usage in client package).
+ *
  * @author Costin Leau
  */
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -58,6 +58,7 @@ public class BasicHBaseTest {
 	String qualifier = "someQualifier";
 	String value = "Some Value";
 
+	@SuppressWarnings({ "deprecation", "resource" })
 	@Test
 	public void testHBaseConnection() throws Exception {
 		HBaseAdmin admin = new HBaseAdmin(config);
@@ -147,7 +148,7 @@ public class BasicHBaseTest {
 		}));
 
 		// manual unbind
-		HTableInterface unboundTable = HbaseSynchronizationManager.unbindResource(tableName);
+		HbaseSynchronizationManager.unbindResource(tableName);
 		HbaseUtils.releaseTable(tableName, t);
 		assertTrue(HbaseSynchronizationManager.getTableNames().isEmpty());
 	}
