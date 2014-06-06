b3f4887043930169f81b19fd59b46ca3f0a9870c
Thomas Risberg <trisberg@gopivotal.com>
2014-06-06 15:47:31 -0400

    SHDP-351 Make JobUtils attempt to get the original job status
    - clean up imports

--- spring-hadoop-core/src/main/java/org/springframework/data/hadoop/mapreduce/JobUtils.java
+++ spring-hadoop-core/src/main/java/org/springframework/data/hadoop/mapreduce/JobUtils.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  * 
- *	  http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -21,7 +21,6 @@ import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.mapred.JobClient;
 import org.apache.hadoop.mapred.JobConf;
 import org.apache.hadoop.mapred.RunningJob;
