4cd426be476e8df96b15f6c4e2e40790e31d45c6
Thomas Risberg <trisberg@pivotal.io>
2015-08-07 13:07:26 -0400

    SHDP-506 Adding a basic Sqoop2 tasklet implementation
    - tasklet takes a Sqoop job id for a pre-defined job and executes it
    
    - counters and status information is stored in the step execution context
    
    - use a local Sqoop2 server for testing and include task enableSqoop2Tests

---
+++ spring-hadoop-build-tests/src/test/java/org/springframework/data/hadoop/batch/sqoop2/Sqoop2TaskletTests.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2015 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.data.hadoop.batch.sqoop2;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.batch.core.ExitStatus;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.StepExecution;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.Collection;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * @author Thomas Risberg
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("/org/springframework/data/hadoop/batch/sqoop2/sqoop2-batch.xml")
+public class Sqoop2TaskletTests {
+
+	@Autowired
+	private AbstractApplicationContext ctx;
+
+	@Test
+	public void testTasklet() throws Exception {
+		JobLauncher jobLauncher = ctx.getBean(JobLauncher.class);
+		Job job = ctx.getBean(Job.class);
+		JobExecution jobExecution = jobLauncher.run(
+				job,
+				new JobParametersBuilder().toJobParameters());
+		Collection<StepExecution> stepExecutions = jobExecution.getStepExecutions();
+		StepExecution sqoop2Step = null;
+		for (StepExecution se : stepExecutions) {
+			if ("sqoop2".equals(se.getStepName())) {
+				sqoop2Step = se;
+			}
+		}
+		assertNotNull(sqoop2Step);
+		assertEquals(ExitStatus.COMPLETED, sqoop2Step.getExitStatus());
+		assertEquals("1", sqoop2Step.getExecutionContext().get("sqoop2.job.id"));
+		assertTrue(sqoop2Step.getExecutionContext().get("sqoop2.external.job.id").toString().length() > 10);
+		assertEquals("SUCCEEDED", sqoop2Step.getExecutionContext().get("sqoop2.job.status"));
+		assertTrue(sqoop2Step.getExecutionContext().get("sqoop2.job.counters").toString().length() > 100);
+		ctx.close();
+	}
+
+}
